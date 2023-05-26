406d4798cf2204a1db7db94667116b6c2f572efa
Dawid <260306@student.pwr.edu.pl>
2023-05-26 18:47:01 +0200

    Test

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -2,6 +2,6 @@ import java.util.Random;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println("tu bedzie symulacjaaaaaaa");
+        System.out.println("tu bedzie symulacjaaaaaaa22");
     }
 }
